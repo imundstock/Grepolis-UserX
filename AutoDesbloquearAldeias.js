2295af78c98b198f763c22e27806dc7f268c568e
imundstock <161329885+imundstock@users.noreply.github.com>
2025-09-16 16:16:58 -0300

    Create AutoDesbloquearAldeias.js

---
+++ AutoDesbloquearAldeias.js
@@ -0,0 +1,57 @@
+(function () {
+    'use strict';
+
+    const uw = typeof unsafeWindow !== 'undefined' ? unsafeWindow : window;
+
+    function unlock(polisID, farmTownPlayerID, ruralID) {
+        let data = {
+            model_url: 'FarmTownPlayerRelation/' + farmTownPlayerID,
+            action_name: 'unlock',
+            arguments: { farm_town_id: ruralID },
+            town_id: polisID
+        };
+        uw.gpAjax.ajaxPost('frontend_bridge', 'execute', data, () => {
+            console.log(`✅ Desbloqueada aldeia ${ruralID} (cidade ${polisID})`);
+        });
+    }
+
+    function desbloquearAldeiasDaIlhaAtual() {
+        let polisID = uw.Game.townId;
+        let islandX = uw.ITowns.towns[polisID].getIslandCoordinateX();
+        let islandY = uw.ITowns.towns[polisID].getIslandCoordinateY();
+
+        let aldeias = uw.MM.getCollections().FarmTown[0].models;
+        let relacoes = uw.MM.getCollections().FarmTownPlayerRelation[0].models;
+
+        for (let i = 0; i < aldeias.length; i++) {
+            let aldeia = aldeias[i];
+            if (aldeia.attributes.island_x === islandX && aldeia.attributes.island_y === islandY) {
+                let ruralID = aldeia.id;
+
+                for (let j = 0; j < relacoes.length; j++) {
+                    let rel = relacoes[j];
+                    if (ruralID === rel.getFarmTownId()) {
+                        let farmTownPlayerID = rel.id;
+
+                        if (rel.attributes.relation_status === 0) {
+                            unlock(polisID, farmTownPlayerID, ruralID);
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const waitUntilReady = setInterval(() => {
+        if (
+            uw.Game?.townId &&
+            uw.ITowns?.towns &&
+            uw.MM?.getCollections()?.FarmTown?.[0]?.models?.length &&
+            uw.MM?.getCollections()?.FarmTownPlayerRelation?.[0]?.models?.length
+        ) {
+            clearInterval(waitUntilReady);
+            console.log("🚀 Desbloqueando aldeias da ilha da cidade atual...");
+            desbloquearAldeiasDaIlhaAtual();
+        }
+    }, 1000);
+})();
